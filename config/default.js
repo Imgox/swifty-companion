a1eb91757ac5083943e0215a78f91397b5cc5b06
Souhaib LAANANI <sohaib.laanani@gmail.com>
2021-10-24 17:21:39 +0100

    Update config.js

--- config/default.js
+++ config/default.js
@@ -1,8 +1,7 @@
 module.exports = {
 	grant_type: "client_credentials",
-	client_id: "a2207c510c1d63054c3ea46fa10cc2907bae82685e3218a5825471d89c63f852",
-	client_secret:
-		"21c8556261c64c3433909bea8a77d5b8b20c51093b761f58c078cd8e90f86d42",
+	client_id: "YOUR_CLIENT_ID",
+	client_secret: "YOUR_CLIENT_SECRET",
 	authorize_uri: "https://api.intra.42.fr/oauth/token",
 	api_path: "https://api.intra.42.fr/v2",
 	token_storage_key: "A01p16I09",
